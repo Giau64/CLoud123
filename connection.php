e5d1c93312d227f10c30fd1f17e17602776dda3b
Giau <giautngcc19047@fpt.edu.vn>
2021-11-08 21:50:11 +0700

    update

---
+++ connection.php
@@ -0,0 +1,4 @@
+<?php 
+    $conn = pg_connect("postgres://rsvlnbxjlwaurl:e32bff6fe212a25552cbb2d1143771ef8867209f638874100ba0b84f46c0dca2@ec2-34-203-182-172.compute-1.amazonaws.com:5432/dd3u8mjuj30jfk");
+            or die("Can not connect database".pg_connect_error());
+?>
