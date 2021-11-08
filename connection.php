4752a9389de85d46d5491a355babfb0658f42ca8
Giau <giautngcc19047@fpt.edu.vn>
2021-11-08 22:03:28 +0700

    upload

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php 
-    $conn = pg_connect("postgres://rsvlnbxjlwaurl:e32bff6fe212a25552cbb2d1143771ef8867209f638874100ba0b84f46c0dca2@ec2-34-203-182-172.compute-1.amazonaws.com:5432/dd3u8mjuj30jfk");
-            or die("Can not connect database".pg_connect_error());
+    $conn = pg_connect("postgres://hqbusrtkreosaq:4c7812db2b808d40e774a9c01882e7e881253bd7f139a9f47cb301c588796440@ec2-54-156-121-167.compute-1.amazonaws.com:5432/d85v0e90ipqggs");
+            or die("Can not connect database".pg_connect_errormessage());
 ?>
